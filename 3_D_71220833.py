55b606fb45a8b4000eda861235096044df8f79df
Christ <117885718+ChristBernardS@users.noreply.github.com>
2023-02-08 18:58:58 +0700

    Add files via upload

--- 3_D_71220833.py
+++ 3_D_71220833.py
@@ -1,10 +1,10 @@
-h = int(input('Masukan Angka: '))
+h_untuk_tinggi_diamond = int(input('Masukan Angka: '))
 
 for i in range(1):
     print(' ')
-    for i in range(h):
-        print(' '*(h-i), '* '*(i+1))
-    for i in range(h-2, -1, -1):
-        print(' '*(h-i), '* '*(i+1))
+    for i in range(h_untuk_tinggi_diamond):
+        print(' '*(h_untuk_tinggi_diamond-i), '* '*(i+1))
+    for i in range(h_untuk_tinggi_diamond-2, -1, -1):
+        print(' '*(h_untuk_tinggi_diamond-i), '* '*(i+1))
 
 print()
